a806d3b892c03574635723db97e334b087a01e64
rama2387 <balantrapuramadevi@gmail.com>
2024-01-20 20:48:57 +0530

    Add project files.

---
+++ iColleague_API/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace iColleague_API.Controllers
+{
+    [Route("api/home/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        string connectionString;
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            connectionString = configuration.GetSection("ConnectionStrings:DefaultConnection").Value;
+        }
+        [HttpGet("getquestions")]
+        public DataSet GetQuestions()
+        {
+
+            SQLConnection cn = new SqlConnection(connectionString);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from tbl_data");
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+
+
+            return ds;
+        }
+
+        [HttpGet("getquestionsbyid")]
+        public DataSet GetQuestionsById(int id)
+        {
+
+            SQLConnection cn = new SqlConnection(connectionString);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from tbl_data where id="+id);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+
+
+            return ds;
+        }
+    }
+}
